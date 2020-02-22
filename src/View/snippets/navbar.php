40b07457f592febd6b1e2454bd521d196edf4ed8
Artem Works <rumiantsev.ca@gmail.com>
2020-02-22 13:43:42 -0500

    more content

--- src/View/snippets/navbar.php
+++ src/View/snippets/navbar.php
@@ -1,4 +1,4 @@
-	
+
 	<nav class="navbar d-flex flex-row bd-highlight fixed-top pt-4 pb-4 bg-white">
 	<div class="container">
 		<div class="d-flex justify-content-start">
@@ -36,7 +36,7 @@
 	        </button>
 	      </div>
 	      <div class="modal-body">
-	        
+
 	      	<div class="container">
 
 	      		<div class="row">
@@ -44,7 +44,7 @@
 	      			<div class="col-md-12">
 
 	      				<h1 class="pt-4">
-	      					<a href="#" class="text-white text-decoration-none projects-font">Projects</a>
+	      					<a href="/projects" class="text-white text-decoration-none projects-font">Projects</a>
 	      				</h1>
 
 	      				<h1 class="pt-4">
@@ -124,10 +124,10 @@
 		      			</span>
 		      			<span class="pr-2 align-middle">
 		      				<a href="https://www.facebook.com/artem.page" rel="noopener" target="_blank" class="text-white"><i class="fab fa-facebook fa-lg"></i></a>
-		      			</span>		 
+		      			</span>
 		      			<span class="pr-2 align-middle">
 		      				<a href="https://www.youtube.com/channel/UCRihv4lcxXDd01vUukIFp5w" rel="noopener" target="_blank" class="text-white"><i class="fab fa-youtube fa-lg"></i></a>
-		      			</span>	     			
+		      			</span>
 		      		</p>
 
 				</div>
@@ -144,16 +144,16 @@
 
 	<div class="modal fade" id="searchModal" tabindex="-1" role="dialog" aria-labelledby="searchModalTitle" aria-hidden="true">
 	  <div class="modal-fullscreen modal-dialog" role="document">
-	    
+
 	    <div class="modal-content p-0 m-0 border-0 rounded-0 bg-dark">
 	      <div class="modal-header border-0 p-5">
 	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
 	          <span aria-hidden="true"><i class="fas fa-times fa-lg text-white"></i></span>
 	        </button>
 	      </div>
-	      
+
 	      <div class="modal-body">
-	        
+
 	      	<div class="container">
 
 	      		<div class="row">
@@ -163,17 +163,17 @@
 						<div class="d-flex justify-content-center search-container">
 
 						<form class="col-md-9">
-						  
+
 							<div class="input-group input-group-lg align-items-center">
-								  
+
 								<input type="text" class="form-control bg-dark border border-white border-top-0 border-right-0 border-left-0 rounded-0 p-0 m-0 text-white" placeholder="Search" aria-label="Search" aria-describedby="button-addon2">
 
 								<div class="input-group-append">
 								    <button class="btn btn-link border border-dark border-top-0 border-right-0 border-left-0 rounded-0 text-white text-decoration-none" type="button" id="button-addon2"><i class="fas fa-search"></i></button>
 								</div>
 
-							</div>    
-							
+							</div>
+
 						</form>
 
 						</div>
